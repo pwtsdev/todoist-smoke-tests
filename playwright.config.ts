69fa6069e1b2c9c7035b1d04a04c6614d37fcc38
bkita <2436425+bkita@users.noreply.github.com>
2024-12-30 13:57:27 +0100

    feat: create login session

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -7,6 +8,8 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export const LOGIN_SESSION = path.join(__dirname, 'tmp/login.json');
 
 export default defineConfig({
   testDir: './tests',
@@ -26,7 +29,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], locale: 'pl-PL', storageState: LOGIN_SESSION },
     },
   ],
 });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,20 @@
 import { expect, test } from '@playwright/test';
 
-test('login to todoist account', async ({ page }) => {
+test.skip('login to todoist account', async ({ page }) => {
   await page.goto('https://app.todoist.com/auth/login');
 
   await page.getByPlaceholder('Wpisz swój e-mail...').fill('bartek@pwts.dev');
   await page.getByPlaceholder('Wpisz hasło...').fill('K!V5n4FJ@G7Hto$jj&7$');
   await page.getByRole('button', { name: 'Logowanie' }).click();
 
+  await expect(page).toHaveURL('https://app.todoist.com/app/today');
+  await expect(page).toHaveTitle(/Dziś/);
+
+  await page.context().storageState({ path: 'tmp/login.json' });
+});
+
+test.skip('should be logged in', async ({ page }) => {
+  await page.goto('https://app.todoist.com/app/today');
   await expect(page).toHaveURL('https://app.todoist.com/app/today');
   await expect(page).toHaveTitle(/Dziś/);
 });
