f3b8eb9fdf7ced39c0a5fd6f4a6b1f3085d97cee
bkita <2436425+bkita@users.noreply.github.com>
2025-01-03 09:55:11 +0100

    feat: add popup handler

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -11,9 +11,21 @@ export abstract class BasePage {
     await this.page.goto(this.url);
     await expect(this.page.getByTestId('app-sidebar-container')).toBeVisible();
     await expect(this.page.getByTestId('task-selection-list-container')).toBeVisible();
+    await this.closeNotificationPopup();
   }
 
   async getPageTitle(): Promise<string> {
     return await this.page.title();
+  }
+
+  protected async closeNotificationPopup(): Promise<void> {
+    const notificationPopup = this.page.getByTestId('toasts-container').locator('div[role="alert"]');
+    await this.page.addLocatorHandler(notificationPopup, async () => {
+      const popupTitle = await notificationPopup.locator('div div').innerText();
+      this.log.info(`🚧 A notification popup has appeared: "${popupTitle}"`);
+      this.log.info('🚫 Closing notification popup!');
+      await notificationPopup.getByLabel('Zamknij').click();
+      await expect(notificationPopup).toBeHidden();
+    });
   }
 }
