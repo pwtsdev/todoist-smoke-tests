9ccbe2a7fc33912aa3ba3d8bf60f821e73303a3b
bkita <2436425+bkita@users.noreply.github.com>
2025-01-03 12:24:11 +0100

    feat: add comment with image

--- src/modals/TaskModal.ts
+++ src/modals/TaskModal.ts
@@ -5,6 +5,9 @@ export class TaskModal extends BaseModal {
   // Locators
   readonly taskModal = (): Locator => this.page.getByTestId('task-details-modal');
   readonly addCommentButton = (): Locator => this.taskModal().getByRole('button', { name: 'Skomentuj' });
+  readonly attachmentButtons = (): Locator => this.taskModal().locator('.comments_textarea_holder button svg');
+  readonly addFileButton = (): Locator => this.attachmentButtons().first();
+  readonly addAudioButton = (): Locator => this.attachmentButtons().nth(2);
 
   // Actions
   async addComment(comment: string): Promise<void> {
@@ -12,6 +15,15 @@ export class TaskModal extends BaseModal {
     await expect(this.addCommentButton()).toBeVisible();
 
     await this.taskModal().getByLabel('Skomentuj').fill(comment);
+
+    // UPLOAD FILE
+    const uploadPromise = this.page.waitForEvent('filechooser');
+    await this.addFileButton().click();
+    const upload = await uploadPromise;
+    await upload.setFiles('uploads/kod.png');
+    await expect(this.taskModal().getByLabel('Usuń załącznik')).toBeVisible();
+    await expect(this.taskModal().getByText(/kod.png/)).toBeVisible();
+
     await this.addCommentButton().click();
 
     const commentLocator = this.taskModal().locator(`div[class="note_content"]:has-text("${comment}")`).first();
