7385a91f2ef81c12dc5062bb1be72d810f4cd6e5
bkita <2436425+bkita@users.noreply.github.com>
2025-04-10 09:51:16 +0200

    fix: update tag name

--- tests/smoke/create-project.spec.ts
+++ tests/smoke/create-project.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('should create a new project', { tag: '@project123' }, async ({ page }) => {
+test('should create a new project', { tag: '@project' }, async ({ page }) => {
   await page.goto('https://app.todoist.com/app/today');
   await expect(page).toHaveURL('https://app.todoist.com/app/today');
   await expect(page).toHaveTitle(/Dziś/);
